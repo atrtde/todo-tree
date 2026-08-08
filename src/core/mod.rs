91fb27a00149451309b3da891b3aab0a02d0e28c
alexandretrotel <trotelalexandre@proton.me>
2026-08-08 15:06:50 +0200

    refactor: rework parser default regex location

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,9 +1,7 @@
-pub mod parser;
 pub mod priority;
 pub mod tags;
 pub mod types;
 
-pub use parser::DEFAULT_REGEX;
 pub use priority::Priority;
 pub use tags::{DEFAULT_TAGS, TagDefinition};
 pub use types::{FileResult, ScanResult, ScanSummary, TodoItem};
