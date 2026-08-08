ab5f5fcbef25c0e10a772360cb16ddb5cc00f65e
alexandretrotel <trotelalexandre@proton.me>
2026-08-08 15:13:57 +0200

    feat: add display.rs in binaries

---
+++ src/bin/todo-tree/app/display.rs
@@ -0,0 +1,19 @@
+use colored::Color;
+use todo_tree::core::Priority;
+
+pub(crate) fn format_duration(ms: u128) -> String {
+    if ms < 1000 {
+        format!("{}ms", ms)
+    } else {
+        format!("{:.2}s", ms as f64 / 1000.0)
+    }
+}
+
+pub(crate) fn priority_to_color(priority: Priority) -> Color {
+    match priority {
+        Priority::Critical => Color::Red,
+        Priority::High => Color::Yellow,
+        Priority::Medium => Color::Cyan,
+        Priority::Low => Color::Green,
+    }
+}
